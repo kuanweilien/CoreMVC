0a330a31b261bd1d07bd3dc9f576aea6a8cbaf09
Kuanwei <lksh9921120@gmail.com>
2022-06-19 10:08:47 +0800

    add MovieModel column Rating

--- CoreMVC/Controllers/MovieModelsController.cs
+++ CoreMVC/Controllers/MovieModelsController.cs
@@ -93,7 +93,7 @@ namespace CoreMVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] MovieModel movieModel)
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] MovieModel movieModel)
         {
             if (ModelState.IsValid)
             {
@@ -127,7 +127,7 @@ namespace CoreMVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] MovieModel movieModel)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] MovieModel movieModel)
         {
             if (id != movieModel.Id)
             {
--- CoreMVC/Models/SeedData.cs
+++ CoreMVC/Models/SeedData.cs
@@ -23,6 +23,7 @@ namespace CoreMVC.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "R",
                         Price = 7.99M
                     },
 
@@ -31,6 +32,7 @@ namespace CoreMVC.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 8.99M
                     },
 
@@ -39,6 +41,7 @@ namespace CoreMVC.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 9.99M
                     },
 
@@ -47,6 +50,7 @@ namespace CoreMVC.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
+                        Rating = "R",
                         Price = 3.99M
                     }
                 );
